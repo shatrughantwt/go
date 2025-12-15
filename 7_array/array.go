a5ea5f4eeed0a5815828b1ed73314a5cc0c3c01b
shatrughantwt <shatrughanm485@gmail.com>
2025-12-15 19:33:15 +0530

    learn array

---
+++ 7_array/array.go
@@ -0,0 +1,23 @@
+package main 
+
+import "fmt"
+
+func main() {
+	// var nums [6]int
+	// fmt.Println(nums)
+
+
+	// nums[0] = 4
+	// nums[2] = 5
+	// fmt.Println(nums)
+
+	// var boole [3]bool
+	// fmt.Println(boole)
+
+
+	// nums := [6]int{1,2,3,4,5,6}
+	// fmt.Println(nums)
+
+	nums := [2][2]int{{3,4},{5,7}}
+	fmt.Println(nums)
+}
