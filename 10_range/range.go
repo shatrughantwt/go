8b816069599374595f16cd68aaea1e5815a7f3b4
shatrughantwt <shatrughanm485@gmail.com>
2025-12-16 19:06:31 +0530

    learn range

---
+++ 10_range/range.go
@@ -0,0 +1,25 @@
+package main 
+
+import "fmt"
+
+func main() {
+	// nums := []int {5,6,7,8}
+
+	// for i := 0; i < len(nums); i++{
+	// 	fmt.Println(nums[i])
+	// }
+
+	// for _, num := range nums {
+	// 	fmt.Println(num)
+	// }
+
+	// sum := 0
+	// for i, num := range nums{
+	// 	// sum = sum + num
+	// 	fmt.Println(i, num)
+	// }
+
+	for i, c := range "boruto"{
+		fmt.Println(i, c)
+	}
+}
