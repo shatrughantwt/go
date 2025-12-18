bac0f685b88b762754a97a6acb9531ab76cbcb73
shatrughan mishra <shatrughanm485@gmail.com>
2025-12-18 19:09:18 +0530

    learn variadic

--- 12_variadic/variadic.go
+++ 12_variadic/variadic.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func sum(nums ...int)int{
+	total := 0
+
+	for _, num := range nums{
+		total = total + num
+	}
+
+	return total
+}
 func main() {
-	fmt.Println("hello in ubuntu")
+	nums := []int{1,2,3,4,5}
+	result := sum(nums...)
+
+	fmt.Println(result)
+	
 }
